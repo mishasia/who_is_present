7bfaff4c6bf307ad962554a119021cb611635a94
mishasia <mishasia2515@gmail.com>
2018-03-24 14:27:27 +0200

    added js listeners

---
+++ app/resources/assets/js/checkbox-listener.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+$(document).ready(function () {
+    const checkbox = $('.checkbox')
+
+    const is_present = checkbox.data('is_present')
+    checkbox.prop('checked', is_present)
+
+    checkbox.change((e) => {
+        const id = checkbox.data('id');
+        axios.post(`/teacher/${id}change-status`).then((response) => {
+            console.log(response)
+        })
+    })
+})
