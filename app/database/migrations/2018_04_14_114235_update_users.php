906cce2bf4d2f793b06edb9d4e5ad59965df322c
mishasia <mishasia2515@gmail.com>
2018-04-15 11:44:19 +0300

    change table of users- add teacher and student

--- app/database/migrations/2018_04_14_114235_update_users.php
+++ app/database/migrations/2018_04_14_114235_update_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTeacherIdToUsers extends Migration
+class UpdateUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -15,12 +15,17 @@ class AddTeacherIdToUsers extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedInteger('teacher_id')->nullable();
-
+            $table->unsignedInteger('student_id')->nullable();
             $table->foreign('teacher_id')
                 ->references('id')
                 ->on('teachers')
                 ->onDelete('cascade');
+            $table->foreign('student_id')
+                ->references('id')
+                ->on('students')
+                ->onDelete('cascade');
         });
+
     }
 
     /**
@@ -32,8 +37,9 @@ class AddTeacherIdToUsers extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropForeign(['teacher_id']);
-            $table->dropIndex('teacher_id');
             $table->dropColumn('teacher_id');
+            $table->dropForeign(['student_id']);
+            $table->dropColumn('student_id');
         });
     }
 }
